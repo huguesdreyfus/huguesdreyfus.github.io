46162b06c5f966a9a0cfa33671e3ec96acf42247
hdreyfus <huguesdreyfus@proton.me>
2026-09-17 17:31:27 +0700

    initial commit

---
+++ back_end/js_functions.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    /*Loads common loader*/
+    $("header").load("/back_end/commons.html #common_header");
+
+    /*Toggles header*/
+    $("#experience button").on("click", function(){
+        $("#experience_text").slideToggle(200);
+        $(this).toggleClass("open")
+    });
+
+    $("#education button").on("click", function(){
+        $("#education_text").slideToggle(200);
+        $(this).toggleClass("open")
+    });
+
+    $("#academic button").on("click", function(){
+        $("#academic_text").slideToggle(200);
+        $(this).toggleClass("open")
+    });
+
+    $("#skills button").on("click", function(){
+        $("#skills_text").slideToggle(200);
+        $(this).toggleClass("open")
+    });
+});
