b4c96f638e6d169944181caca0764cbe1fc70b79
hdreyfus <huguesdreyfus@proton.me>
2026-09-18 11:16:18 +0700

    simplified toggling, updated hard skills

--- back_end/js_functions.js
+++ back_end/js_functions.js
@@ -2,31 +2,16 @@ $(document).ready(function(){
     /*Loads common loader*/
     $("header").load("/back_end/commons.html #common_header");
 
-    /*Toggles header*/
-    $("#experience button").on("click", function(){
-        $("#experience_text").slideToggle(200);
-        $(this).toggleClass("open")
+    /*Toggling*/
+    
+    $(".topic_title").on("click", function(){
+        $(this).toggleClass("expanded")
+        $(this).nextUntil(":not(.topic_text)", ".topic_text").slideToggle(200);
     });
 
-    $("#education button").on("click", function(){
-        $("#education_text").slideToggle(200);
-        $(this).toggleClass("open")
+    $(".parent_tag").on("click", function() {
+        $(this).toggleClass("expanded")
+        $(this).nextUntil(":not(.child_tag)", ".child_tag").toggleClass("hidden");
     });
-
-    $("#academic button").on("click", function(){
-        $("#academic_text").slideToggle(200);
-        $(this).toggleClass("open")
-    });
-
-    $("#skills button").on("click", function(){
-        $("#skills_text").slideToggle(200);
-        $(this).toggleClass("open")
-    });
-
-    $('.parent_tag').on('click', function() {
-        $(this).nextUntil(':not(.child_tag)', '.child_tag').toggleClass('hidden');
-    });
-
-
 
 });
